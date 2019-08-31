d5eb6ca52f54354eee191553e1b3459d82de6ecc
Yoni Meller <yonim1857@gmail.com>
2019-08-31 16:46:56 +0300

    Updated the version

--- src/main/java/com/smodpliveteam/smodplive/util/reference.java
+++ src/main/java/com/smodpliveteam/smodplive/util/reference.java
@@ -1,9 +1,12 @@
 package com.smodpliveteam.smodplive.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class reference {
     public static final String MOD_ID = "smplive";
     public static final String NAME = "SModP Live";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "ALPHA DEVELOPMENT BUILD";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
     public static final String CLIENT_PROXY_CLASS = "com.smodpliveteam.smodplive.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "com.smodpliveteam.smodplive.proxy.CommonProxy";
